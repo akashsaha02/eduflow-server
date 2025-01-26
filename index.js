032f48b032f2bccd30da236323967f070137562a
tusherbhakta <tusheranta@gmail.com>
2025-01-26 22:15:04 +0530

    set up Express server with CORS, JSON parsing, and MongoDB connection

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 const express = require('express');
 const app = express();
+
+const cors = require('cors');
+require('dotenv').config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const port = process.env.PORT || 5000;
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+
+const uri = process.env.MONGO_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
